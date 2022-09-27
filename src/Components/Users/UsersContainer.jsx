4f68d0975b0225aedd5b3a271eec5c363a2896f6
Ihor <dimov.igor97@gmail.com>
2022-09-27 18:06:44 +0300

    55 (Pagination)

--- src/Components/Users/UsersContainer.jsx
+++ src/Components/Users/UsersContainer.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import {connect} from "react-redux";
-import {followAC, setUsersAC, unfollowAC} from "../../Redux/users-reducer";
+import {followAC, setCurrentPageAC, setUsersAC, setUsersTotalCountAC, unfollowAC} from "../../Redux/users-reducer";
 import {Users} from "./Users";
 
 let mapStateToProps = (state) => {
     return {
-        users:state.usersPage.users
+        users:state.usersPage.users,
+        pageSize:state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage:state.usersPage.currentPage,
     }
 }
 let mapDispatchToProps = (dispatch) => {
@@ -19,6 +22,12 @@ let mapDispatchToProps = (dispatch) => {
         setUsers:(users)=> {
             dispatch(setUsersAC(users))
         },
+        setCurrentPage:(pageNumber)=> {
+            dispatch(setCurrentPageAC(pageNumber))
+        },
+        setTotalUsersCount:(totalCount)=> {
+            dispatch(setUsersTotalCountAC(totalCount))
+        }
     }
 }
 
--- src/Redux/users-reducer.js
+++ src/Redux/users-reducer.js
@@ -1,13 +1,18 @@
 const FOLLOW = 'FOLLOW'
 const UNFOLLOW = 'UNFOLLOW'
 const SET_USERS = 'SET_USERS'
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE'
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT'
 
 let initialState = {
     users: [
         /*{id:1,photo:'http://cdn.onlinewebfonts.com/svg/img_264570.png',followed:false,fullName:'Ihor',status:'i am a boss',location:{city:'Minsk',country:'Belarus'}},
         {id:2,photo:'https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png',followed:true,fullName:'Max',status:'i am a worker',location:{city:'Moscow',country:'Russia'}},
         {id:3,photo:'https://cdn-icons-png.flaticon.com/512/1053/1053244.png?w=360',followed:false,fullName:'Ivan',status:'i am a driver',location:{city:'Kiev',country:'Ukraine'}},*/
-    ]
+    ],
+    pageSize: 5,
+    totalUsersCount: 0,
+    currentPage:1,
 }
 
 export const usersReducer = (state=initialState, action)=> {
@@ -35,7 +40,13 @@ export const usersReducer = (state=initialState, action)=> {
             }
         }
         case SET_USERS: {
-            return {...state,users: [...state.users , ...action.users]}
+            return {...state,users: action.users}
+        }
+        case SET_CURRENT_PAGE: {
+            return {...state,currentPage: action.currentPage}
+        }
+        case SET_TOTAL_USERS_COUNT: {
+            return {...state,totalUsersCount: action.count}
         }
         default:
             return state
@@ -46,3 +57,5 @@ export const usersReducer = (state=initialState, action)=> {
 export const followAC = (userId) => ({type:FOLLOW, userId})
 export const unfollowAC = (userId) => ({type:UNFOLLOW, userId})
 export const setUsersAC = (users) => ({type:SET_USERS, users})
+export const setCurrentPageAC = (currentPage) => ({type:SET_CURRENT_PAGE, currentPage})
+export const setUsersTotalCountAC = (totalUsersCount) => ({type:SET_TOTAL_USERS_COUNT, count:totalUsersCount})
